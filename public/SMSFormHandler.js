8574f58dc76b845549f2102767f6a82a1e35e7a6
Gang Fu <ganfu@amazon.com>
2020-04-30 12:20:40 -0700

    add Authorization header in ajax call

--- public/SMSFormHandler.js
+++ public/SMSFormHandler.js
@@ -43,10 +43,18 @@ $(document).ready(function() {
         'optTimestamp': timestamp.toString()
       });
 
+      var authUser = storage.getItem('CognitoIdentityServiceProvider.18qa4ijulum1ui5jaiqc1d4drl.LastAuthUser');
+      console.log(authUser);
+      var idTokenKey = 'CognitoIdentityServiceProvider.18qa4ijulum1ui5jaiqc1d4drl.' + authUser + '.idToken';
+      console.log(idTokenKey);
+      var idTokenValue = storage.getItem(idTokenKey);
+
       $.ajax({
         type: 'POST',
         url: 'https://5tve7pk601.execute-api.us-west-2.amazonaws.com/call/',
-        
+        headers: {
+          "Authorization": idTokenValue
+        },
         data: data,
         success: function(res) {
           $('#form-response').html('<div class="mt-3 alert alert-success" role="alert"><p>Congratulations! You&apos;ve successfully registered for SMS Alerts from ExampleCorp.</p><p>We just sent you a message. Follow the instructions in the message to confirm your subscription. We won&apos;t send any additional messages until we receive your confirmation.</p><p>If you decide you don&apos;t want to receive any additional messages from us, just reply to one of our messages with the keyword STOP.</p></div>');
